e5085d23485b388d736ee4d9d43d25d60cfe8f6e
CJ-Cepe <128797416+CJ-Cepe@users.noreply.github.com>
2023-12-06 01:00:59 +0800

    Build: code and testing

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
-
-function capitalize(val){
-  val[0] = val[0].toUpperCase();
-  return val
+function capitalize(val) {
+  return val[0].toUpperCase() + val.slice(1);
 }
 
-export {capitalize}
+function reverse(val) {
+  let newVal = '';
+  for (let i = val.length - 1; i >= 0; i -= 1) {
+    newVal += val[i];
+  }
+  return newVal;
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function analyzeArray(nums) {
+  const { length } = nums;
+  let min = nums[0];
+  let max = nums[0];
+  let average = nums[0];
+
+  for (let i = 1; i < nums.length; i += 1) {
+    min = min > nums[i] ? nums[i] : min;
+    max = max > nums[i] ? max : nums[i];
+    average += nums[i];
+  }
+
+  average = Math.ceil(average / length);
+
+  return {
+    length, min, max, average,
+  };
+}
+export {
+  capitalize, reverse, add, subtract, multiply, divide, analyzeArray,
+};
--- index.test.js
+++ index.test.js
@@ -1,15 +1,59 @@
+import { capitalize, reverse, add, subtract, multiply, divide, analyzeArray} from './index';
 
+describe('Testing Practice', () => {
+  /*
+  A capitalize function that takes a string and
+    returns it with the first character capitalized.
+  */
+  test('Capitalize first character', () => {
+    expect(capitalize('watermelon')).toEqual('Watermelon');
+  });
 
-/* 
-A capitalize function that takes a string and 
-  returns it with the first character capitalized.
-*/
+  /*
+  A reverseString function that takes a string and
+    returns it reversed.
+  */
+  test('Reverse string', () => {
+    expect(reverse('watermelon')).toEqual('nolemretaw');
+  });
 
-test.todo('capitalize');
+  /*
+  An analyzeArray function that takes an array of numbers 
+    and returns an object with the following properties: 
+    average, min, max, and length.
+  */
+  test('Analyze array', ()=> {
+    expect(analyzeArray([1,8,3,4,2,6])).toEqual({
+      average: 4,
+      min: 1,
+      max: 8,
+      length: 6
+      });
+    })
+  });
 
-/*
-A reverseString function that takes a string and 
-  returns it reversed.
-*/
+describe('Calculation', ()=>{
+  let a = 20, b = 10;
 
-test.todo('reverse string')
+  /*
+  A calculator object that contains functions for the basic 
+    operations: add, subtract, divide, and multiply. Each of 
+    these functions should take two numbers and return the 
+    correct calculation.
+  */
+  test('Addition', () => {
+    expect(add(a, b)).toBe(30)
+  }) 
+
+  test('Subtraction', ()=> {
+    expect(subtract(a, b)).toBe(10)
+  });
+
+  test('Multiplication', ()=> {
+    expect(multiply(a, b)).toBe(200)
+  });
+
+  test('Division', ()=> {
+    expect(divide(a, b)).toBe(2)
+  });
+})
